c8d026c0bf3c19b1841fd011719ec3ef3b19e640
davecheng-tech <dave.cheng@ycdsbk12.ca>
2025-12-16 10:17:36 -0500

    Add bubble-on-click, bubble animation, bubble array

--- src/Sketch.java
+++ src/Sketch.java
@@ -1,7 +1,9 @@
 import processing.core.PApplet;
 import processing.core.PImage;
+import java.util.ArrayList;
 
 public class Sketch extends PApplet {
+
     PImage fishLeft;
     PImage fishRight;
 
@@ -10,6 +12,11 @@ public class Sketch extends PApplet {
     float fishSpeed;
 
     boolean isMovingRight;
+
+    boolean debug = false;
+
+    ArrayList<int[]> bubbles;
+    int bubbleSpeed = 3;
 
     public static void main(String[] args) {
         PApplet.main("Sketch");
@@ -31,6 +38,8 @@ public class Sketch extends PApplet {
 
         isMovingRight = true;
 
+        bubbles = new ArrayList<int[]>();
+
         imageMode(CENTER);
     }
 
@@ -40,6 +49,9 @@ public class Sketch extends PApplet {
 
         moveFish();
         displayFish();
+
+        updateBubbles();
+        displayBubbles();
     }
 
     public void moveFish() {
@@ -47,6 +59,7 @@ public class Sketch extends PApplet {
             fishX += fishSpeed;
 
             if (fishX > width - fishRight.width / 2) {
+                fishX = width - fishRight.width / 2;
                 isMovingRight = false;
             }
 
@@ -54,6 +67,7 @@ public class Sketch extends PApplet {
             fishX -= fishSpeed;
 
             if (fishX < fishLeft.width / 2) {
+                fishX = fishLeft.width / 2;
                 isMovingRight = true;
             }
         }
@@ -66,4 +80,59 @@ public class Sketch extends PApplet {
             image(fishLeft, fishX, fishY);
         }
     }
+
+    @Override
+    public void mousePressed() {
+        int bubbleX;
+
+        if (isMovingRight) {
+            bubbleX = (int) (fishX + fishRight.width / 2);
+        } else {
+            bubbleX = (int) (fishX - fishLeft.width / 2);
+        }
+
+        int bubbleY = (int) fishY;
+
+        int[] bubble = { bubbleX, bubbleY };
+        bubbles.add(bubble);
+
+        if (debug) {
+            debugBubbles();
+        }
+        
+    }
+
+    public void updateBubbles() {
+        for (int i = bubbles.size() - 1; i >= 0; i--) {
+            int[] bubble = bubbles.get(i);
+
+            bubble[1] -= bubbleSpeed;
+
+            if (bubble[1] <= 0) {
+                bubbles.remove(i);
+            }
+        }
+    }
+
+    public void displayBubbles() {
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+
+        for (int[] bubble : bubbles) {
+            ellipse(bubble[0], bubble[1], 10, 10);
+        }
+    }
+
+    public void debugBubbles() {
+        System.out.println("Number of bubbles: " + bubbles.size());
+
+        for (int i = 0; i < bubbles.size(); i++) {
+            int[] bubble = bubbles.get(i);
+            System.out.println("Bubble " + i + " -> x: " + bubble[0] + ", y: " + bubble[1]);
+        }
+
+        System.out.println("----------------------------");
+    }
+
 }
